158461d41dceb8bdd2d5fdff540bb201c19ba4ba
Torin Blankensmith <torin.blankensmith@gmail.com>
2015-10-01 19:20:42 -0700

    Final HW1

---
+++ HW1/HW1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by blankens on 10/1/15.
+//  Copyright © 2015 Adobe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
